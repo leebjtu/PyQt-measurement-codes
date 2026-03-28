c019351f4dc2bd25ef431fd5dde83dbfa1dc7cd2
NJITer <49837144+leebjtu@users.noreply.github.com>
2026-03-28 16:43:16 +0800

    Delete Chapter08 directory

--- Chapter08/01pyserial基础测试_代码8-3/main.py
+++
@@ -1,7 +0,0 @@
-import serial
-if __name__ == '__main__':
-    ser = serial.Serial("com3", 9600, timeout=1)
-    ser.write(b'hello')
-    ser.write('hello123'.encode('utf-8'))
-    ser.readall()
-    ser.close()
